9e8321dc329882aee0142efd7be797e9c63ff9b1
StaRen <starendev@gmail.com>
2020-08-27 12:57:52 +0300

    added full ie11 support

---
+++ src/js/helpers.js
@@ -0,0 +1,43 @@
+// because scrollintoview behavior smooth works only in chrome opera and firefox, i had to implement this duct tape
+// to get it working in microsoft edge etc. code snippet from stackoverflow.com/questions/51599677
+export function scrollToSmoothly(pos, time){
+    /*Time is only applicable for scrolling upwards*/
+    /*Code written by hev1*/
+    /*pos is the y-position to scroll to (in pixels)*/
+    if(isNaN(pos)){
+        throw "Position must be a number";
+    }
+    if(pos<0){
+        throw "Position can not be negative";
+    }
+    let currentPos = window.scrollY||window.screenTop;
+    if(currentPos<pos){
+        let t = 10;
+        for(let i = currentPos; i <= pos; i+=10){
+            t+=10;
+            setTimeout(function(){
+                window.scrollTo(0, i);
+            }, t/2);
+        }
+    } else {
+        time = time || 2;
+        let i = currentPos;
+        let x;
+        x = setInterval(function(){
+            window.scrollTo(0, i);
+            i -= 10;
+            if(i<=pos){
+                clearInterval(x);
+            }
+        }, time);
+    }
+}
+//forEach polyfill for ie11
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (let i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
