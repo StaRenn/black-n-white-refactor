2c42389b3f69466360f8f52afe56386784791284
StaRen <starendev@gmail.com>
2020-08-27 13:40:06 +0300

    added full ie11 support

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
                     fallback: "style-loader",
                     use: [
                         {
-                            loader: "css-loader?url=false"
+                            loader: "css-loader?url=true"
                         },
                         {
                             loader: "postcss-loader",
@@ -46,9 +46,12 @@ module.exports = {
                                     })
                                 ]
                             }
-                        }
+                        },
                     ]
             })},
+            {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                loader: 'url-loader?limit=100000' }
         ],
     },
     plugins: [
